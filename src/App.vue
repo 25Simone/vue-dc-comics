566f3aaea41c933d692d9907da5d8bd2996f7e7b
25Simone <dantsimone111@gmail.com>
2022-01-27 15:10:11 +0100

    set master.scss, variables.scss and header done

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,19 @@
 <template>
   <div id="app">
+    <header-box />
   </div>
 </template>
 
 <script>
-
+import HeaderBox from './components/HeaderBox.vue'
 export default {
   name: 'App',
   components: {
-
+    HeaderBox,
   }
 }
 </script>
 
 <style lang="scss">
-#app {
-
-}
+@import './style/main.scss';
 </style>
---
+++ src/components/HeaderBox.vue
@@ -0,0 +1,51 @@
+<template>
+    <header>
+        <div id="logo">
+            <img src="@/assets/dc-logo.png" alt="logo Dc Comics">
+        </div>
+        <nav>
+            <ul>
+                <li>CHARACTERS</li>
+                <li>COMICS</li>
+                <li>MOVIES</li>
+                <li>TV</li>
+                <li>GAMES</li>
+                <li>COLLECTIBLES</li>
+                <li>VIDEOS</li>
+                <li>FANS</li>
+                <li>NEWS</li>
+                <li>SHOP</li>
+            </ul>
+        </nav>
+    </header>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style lang="scss" scoped>
+@import '@/style/variables.scss';
+
+header{
+    background: #fff;
+    display: flex;
+    justify-content: space-around;
+    align-items: center;
+    padding: 10px;
+    img{
+        width: 85px;
+    }
+    ul{
+        display: flex;
+        color: $text-primary-color;
+        li{
+            padding: 15px;
+            font-size: 15px;
+            cursor: pointer;
+        }
+    }
+}
+</style>
